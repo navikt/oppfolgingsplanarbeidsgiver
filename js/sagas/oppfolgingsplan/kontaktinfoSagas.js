d349ed356bd184a5f95eed2cc569fc7635ce82e8
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-11-21 14:57:05 +0100

    Change kontaktinfoSagas from restoppfoelgingsdialog -> syfooprest

--- js/sagas/oppfolgingsplan/kontaktinfoSagas.js
+++ js/sagas/oppfolgingsplan/kontaktinfoSagas.js
@@ -1,11 +1,14 @@
 import { call, put, fork, takeEvery } from 'redux-saga/effects';
 import { get, log } from '@navikt/digisyfo-npm';
 import * as actions from '../../actions/oppfolgingsplan/kontaktinfo_actions';
+import { fullNaisUrl } from '../../utils/urlUtils';
+import { HOST_NAMES } from '../../konstanter';
 
 export function* hentKontaktinfoSaga(action) {
     yield put(actions.henterKontaktinfo(action.fnr));
     try {
-        const url = `${process.env.REACT_APP_OPPFOELGINGSDIALOGREST_ROOT}/kontaktinfo/${action.fnr}`;
+        const path = `${process.env.REACT_APP_SYFOOPREST_ROOT}/kontaktinfo/${action.fnr}`;
+        const url = fullNaisUrl(HOST_NAMES.SYFOOPREST, path);
         const kontaktinfo = yield call(get, url);
         yield put(actions.kontaktinfoHentet(kontaktinfo, action.fnr));
     } catch (e) {
