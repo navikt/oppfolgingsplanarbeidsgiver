61c679d9423c2ef78e6be216d753e0a059d8ae5f
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-11-27 15:14:32 +0100

    Replace Syfotekster with component texts in OppfolgingsplanFilm

--- js/components/oppfolgingsplaner/OppfolgingsplanFilm.js
+++ js/components/oppfolgingsplaner/OppfolgingsplanFilm.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import {
-    getHtmlLedetekst,
-    getLedetekst,
-} from '@navikt/digisyfo-npm';
 import Video from '../app/Video';
 import { OPPFOLGINGSPLAN } from '../../enums/filmer';
+
+const texts = {
+    title: 'Om oppfølgingsplanen',
+};
+
+const TextLink = () => {
+    return (
+        <React.Fragment>
+            Har du noen spørsmål? <a className="lenke" target="_blank" rel="noopener noreferrer" href="https://nav.no/oppfølgingsplan">Les gjerne mer om oppfølgingsplanen her</a>.
+        </React.Fragment>
+    );
+};
 
 const OppfolgingsplanFilm = () => {
     return (<div className="panel">
         <h2 className="panel__tittel">
-            {getLedetekst('oppfolgingsdialog.filmsnutt.tittel')}
+            {texts.title}
         </h2>
         <Video film={OPPFOLGINGSPLAN} />
-        <p dangerouslySetInnerHTML={getHtmlLedetekst('oppfolgingsdialog.filmsnutt.tekst')} />
+        <p>
+            <TextLink />
+        </p>
     </div>);
 };
 
