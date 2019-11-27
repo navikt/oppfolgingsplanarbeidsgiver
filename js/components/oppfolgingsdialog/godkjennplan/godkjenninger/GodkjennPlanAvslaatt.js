2fbf02f867335baeaa30c8d12e3b90091b0fa6bc
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-11-27 15:40:59 +0100

    Replace Syfotekster with component texts in GodkjennPlanAvslaatt

--- js/components/oppfolgingsdialog/godkjennplan/godkjenninger/GodkjennPlanAvslaatt.js
+++ js/components/oppfolgingsdialog/godkjennplan/godkjenninger/GodkjennPlanAvslaatt.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Hovedknapp } from 'nav-frontend-knapper';
-import { getLedetekst } from '@navikt/digisyfo-npm';
 import OppfolgingsplanInnholdboks from '../../../app/OppfolgingsplanInnholdboks';
 import { oppfolgingsdialogPt } from '../../../../proptypes/opproptypes';
 
+const texts = {
+    title: 'Medarbeideren din er visst ikke helt enig',
+    paragraphInfo: 'Du kan gjøre endringer slik at dere får en god plan.',
+    buttonMakeChanges: 'Rediger planen',
+};
+
 const GodkjennPlanAvslaatt = ({ oppfolgingsdialog, rootUrl, nullstillGodkjenning }) => {
-    const infoboksTekst = getLedetekst('oppfolgingsdialog.arbeidsgiver.godkjennplan.avslaatt.infoboks.tekst');
     return (
         <OppfolgingsplanInnholdboks
             svgUrl={`${rootUrl}/img/svg/oppfolgingsplan-avslaatt.svg`}
             svgAlt="avslaatt"
-            tittel={getLedetekst('oppfolgingsdialog.arbeidsgiver.godkjennplan.avslaatt.infoboks.tittel')}
+            tittel={texts.title}
         >
             <div className="godkjennPlanAvslaatt">
                 <div className="godkjennPlanAvslaatt__infoboks">
-                    <p>{infoboksTekst}</p>
+                    <p>{texts.paragraphInfo}</p>
                 </div>
                 <div className="knapperad">
                     <Hovedknapp onClick={() => { nullstillGodkjenning(oppfolgingsdialog.id, oppfolgingsdialog.arbeidstaker.fnr); }}>
-                        {getLedetekst('oppfolgingsdialog.knapp.rediger-plan')}
+                        {texts.buttonMakeChanges}
                     </Hovedknapp>
                 </div>
             </div>
