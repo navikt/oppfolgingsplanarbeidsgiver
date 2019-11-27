60246ff34c720d7719139f0e7371c9b345279249
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-11-27 14:48:24 +0100

    Replace Syfotekster with component texts in OppfolgingsplanerIngenplan

--- js/components/oppfolgingsdialog/opprett/OppfolgingsplanerIngenplan.js
+++ js/components/oppfolgingsdialog/opprett/OppfolgingsplanerIngenplan.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { getLedetekst } from '@navikt/digisyfo-npm';
 import { getContextRoot } from '../../../routers/paths';
 import { oppfolgingsdialogPt } from '../../../proptypes/opproptypes';
 import OppfolgingsplanerIngenplanKnapper from './OppfolgingsplanerIngenplanKnapper';
+
+const texts = {
+    title: 'Aktiv oppfølgingsplan',
+    panelTitle: 'Du har ingen aktiv oppfølgingsplan',
+    panelInfo: 'Du kan når som helst lage en ny plan. Her legger du inn arbeidsoppgaver, de tiltakene som skal gjelde, og når de skal gjelde for.',
+};
 
 const OppfolgingsplanerIngenplan = (
     {
@@ -13,7 +18,7 @@ const OppfolgingsplanerIngenplan = (
     }) => {
     return (<div className="oppfolgingsdialogerIngenplan">
         <header className="oppfolgingsdialogerIngenplan__header">
-            <h2>{getLedetekst('oppfolgingsdialoger.oppfolgingsdialoger.header.tittel')}</h2>
+            <h2>{texts.title}</h2>
         </header>
         <div className="oppfolgingsdialogerIngenplan__blokk">
             <img alt="tom-plan" src={`${getContextRoot()}/img/svg/oppfolgingsdialog-tom.svg`} />
@@ -21,13 +26,13 @@ const OppfolgingsplanerIngenplan = (
                 <header className="inngangspanel__header">
                     <h3 className="js-title">
                         <span className="inngangspanel__tittel">
-                            {getLedetekst('oppfolgingsdialoger.oppfolgingsdialoger.ingenplan.tittel')}
+                            {texts.panelTitle}
                         </span>
                     </h3>
                 </header>
                 <div>
                     <p className="oppfolgingsdialoger__start_tekst">
-                        {getLedetekst('oppfolgingsdialoger.oppfolgingsdialoger.ingenplan.tekst')}
+                        {texts.panelInfo}
                     </p>
                     <OppfolgingsplanerIngenplanKnapper
                         oppfolgingsdialoger={oppfolgingsdialoger}
