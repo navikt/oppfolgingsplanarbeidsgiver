7c49d23c75685a8c49cabca4f0a30cc324f932e3
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-11-27 14:04:49 +0100

    Replace Syfotekster with component texts in Samtykke

--- js/components/oppfolgingsdialog/godkjennplan/samtykke/Samtykke.js
+++ js/components/oppfolgingsdialog/godkjennplan/samtykke/Samtykke.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Hovedknapp } from 'nav-frontend-knapper';
-import { getLedetekst, getHtmlLedetekst } from '@navikt/digisyfo-npm';
 import { oppfolgingsdialogPt } from '../../../../proptypes/opproptypes';
 import { getContextRoot } from '../../../../routers/paths';
+
+const texts = {
+    title: 'Vil du gjøre det lettere for andre som skal fylle ut?',
+    approve: {
+        yes: 'Ja, jeg samtykker til at dere kan bruke det jeg skriver til å lage statistikk og komme med forslag',
+        no: 'Nei, ikke bruk det jeg skriver',
+    },
+    info: {
+        paragraph1: `Vi ønsker at oppfølgingsplanen skal bli nyttig for sykmeldte og arbeidsgivere.
+        Derfor samler vi inn tiltak fra oppfølgingsplanene og lager statistikk over hva som kan fungere på arbeidsplassen.
+        Slik kan vi komme med forslag når noen fyller ut en ny oppfølgingsplan.`,
+        paragraph2: 'Når vi samler inn opplysningene, kan verken vi eller andre spore disse tilbake til deg fordi opplysningene er blitt helt anonymisert.',
+        paragraph3: 'Det har ingen konsekvenser for deg å si nei til at vi kan bruke det du skriver.',
+    },
+    buttonSubmit: 'Velg',
+};
+
+const InfoText = () => {
+    return (
+        <React.Fragment>
+            {texts.info.paragraph1}
+            <br />
+            <br />
+            {texts.info.paragraph2}
+            <br />
+            <br />
+            {texts.info.paragraph3}
+        </React.Fragment>
+    );
+};
 
 class Samtykke extends Component {
     constructor(props) {
@@ -31,10 +60,12 @@ class Samtykke extends Component {
             <div className="illustrertTittel">
                 <img className="illustrertTittel__img" src={`${getContextRoot()}/img/svg/samtykke-illustrasjon.svg`} alt="samtykke" />
                 <h2 className="illustrertTittel__tittel">
-                    {getLedetekst('oppfolgingsdialog.samtykke.tittel')}
+                    {texts.title}
                 </h2>
             </div>
-            <p className="samtykke__tekst" dangerouslySetInnerHTML={getHtmlLedetekst('oppfolgingsdialog.samtykke.tekst')} />
+            <p className="samtykke__tekst">
+                <InfoText />
+            </p>
             <div className="inputgruppe inputgruppe--samtykke">
                 <div tabIndex="-1" id="samtykkeGitt" className="skjema__feilomrade">
                     <div className="inputgruppe">
@@ -50,7 +81,7 @@ class Samtykke extends Component {
                             <label
                                 className="skjemaelement__label"
                                 htmlFor="giSamtykke">
-                                {getLedetekst('oppfolgingsdialog.samtykke.radioknapp.gi-samtykke.tekst')}
+                                {texts.approve.yes}
                             </label>
                         </div>
                         <div className="skjemaelement">
@@ -65,7 +96,7 @@ class Samtykke extends Component {
                             <label
                                 className="skjemaelement__label ikkeGiSamtykke"
                                 htmlFor="ikkeGiSamtykke">
-                                {getLedetekst('oppfolgingsdialog.samtykke.radioknapp.ikke-gi-samtykke.tekst')}
+                                {texts.approve.no}
                             </label>
                         </div>
                     </div>
@@ -77,7 +108,7 @@ class Samtykke extends Component {
                         htmlType="submit"
                         onClick={this.sendSamtykke}
                         disabled={this.state.samtykke === null}>
-                        {getLedetekst('oppfolgingsdialog.samtykke.knapp.samtykk')}
+                        {texts.buttonSubmit}
                     </Hovedknapp>
                 </div>
             </div>
