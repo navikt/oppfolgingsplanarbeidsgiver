46f2c40ad8636f04deaeae118308fae61628909b
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-11-27 15:16:04 +0100

    Replace Syfotekster with component texts in BaserTidligereSkjemaKomponent

--- js/components/oppfolgingsplaner/opprett/BaserTidligereSkjema.js
+++ js/components/oppfolgingsplaner/opprett/BaserTidligereSkjema.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Field, reduxForm } from 'redux-form';
+import {
+    Field,
+    reduxForm,
+} from 'redux-form';
 import { Hovedknapp } from 'nav-frontend-knapper';
-import { getLedetekst } from '@navikt/digisyfo-npm';
 import Radioknapper from '../../../skjema/Radioknapper';
+
+const texts = {
+    question: 'Ønsker du å basere den nye planen på den som gjaldt sist?',
+    answer: {
+        yes: 'Ja, ta utgangspunkt i den tidligere planen',
+        no: 'Nei, start en ny plan der vi ikke har fylt ut noe',
+    },
+    buttonSubmit: 'Start',
+};
 
 export const BaserTidligereSkjemaKomponent = (
     {
@@ -11,25 +22,25 @@ export const BaserTidligereSkjemaKomponent = (
     }) => {
     return (<form onSubmit={handleSubmit} className="baserTidligereSkjema">
         <label className="skjemaelement__label">
-            {getLedetekst('oppfolgingsdialog.baserTidligereSkjema.spoersmaal')}
+            {texts.question}
         </label>
         <Field
             name="baserPaaTidligerePlan"
             component={Radioknapper}>
             <input
                 value
-                label={getLedetekst('oppfolgingsdialog.baserTidligereSkjema.svar.ja')}
+                label={texts.answer.yes}
                 aria-labelledby="baserPaaTidligerePlan-overskrift"
             />
             <input
                 value={false}
-                label={getLedetekst('oppfolgingsdialog.baserTidligereSkjema.svar.nei')}
+                label={texts.answer.no}
                 aria-labelledby="baserPaaTidligerePlan-overskrift"
             />
         </Field>
         <div className="knapperad">
             <Hovedknapp>
-                {getLedetekst('oppfolgingsdialog.baserTidligereSkjema.knapp.send')}
+                {texts.buttonSubmit}
             </Hovedknapp>
         </div>
     </form>);
