2bf76da7cb9501bf70da673ae29a24ae4ec7d1a2
Mathias Rørvik <mathias.fris.rorvik@nav.no>
2019-12-09 13:54:01 +0100

    change design of back button

--- js/components/oppfolgingsdialog/NavigasjonsBunn.js
+++ js/components/oppfolgingsdialog/NavigasjonsBunn.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
+import styled from 'styled-components';
 import {
     Knapp,
     Hovedknapp,
 } from 'nav-frontend-knapper';
+import { VenstreChevron } from 'nav-frontend-chevron';
 import { getContextRoot } from '../../routers/paths';
 
 const tekster = {
@@ -22,6 +24,27 @@ const handleKeyPress = (settAktivtSteg, nesteSteg, e) => {
     }
 };
 
+const StyledLink = styled(Link)`
+    display: flex;
+    font-weight: bold;
+    align-items: center;
+`;
+
+const BackToOversikt = ({ koblingId }) => {
+    return (
+        <nav>
+            <StyledLink to={`${getContextRoot()}/${koblingId}/oppfolgingsplaner`}>
+                <VenstreChevron />
+                <span>{tekster.knapp.oversikt}</span>
+            </StyledLink>
+        </nav>
+    );
+};
+
+BackToOversikt.propTypes = {
+    koblingId: PropTypes.string,
+};
+
 const NavigasjonsBunn = (
     {
         steg,
@@ -30,11 +53,7 @@ const NavigasjonsBunn = (
         koblingId,
     }) => {
     if (disabled) {
-        return (<nav className="navigasjonsBunn">
-            <Link className="knapp knapp--standard" to={`${getContextRoot()}/${koblingId}/oppfolgingsplaner`}>
-                {tekster.knapp.oversikt}
-            </Link>
-        </nav>);
+        return <BackToOversikt koblingId={koblingId} />;
     }
     return (
         <nav className="navigasjonsBunn">
