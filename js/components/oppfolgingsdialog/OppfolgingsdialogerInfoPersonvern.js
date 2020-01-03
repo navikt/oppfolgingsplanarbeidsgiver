95f1816407768860a2bc7928579d691297f62e54
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-01-03 09:32:45 +0100

    Fix typo in OppfolgingsdialogerInfoPersonvern

--- js/components/oppfolgingsdialog/OppfolgingsdialogerInfoPersonvern.js
+++ js/components/oppfolgingsdialog/OppfolgingsdialogerInfoPersonvern.js
@@ -4,7 +4,7 @@ const texts = {
     paragraphInfo: `
         Oppfølgingsplanen skal gjøre det lettere for arbeidstakeren å bli i jobben.
         Du og arbeidstakeren lager planen sammen og skriver inn opplysninger fra hver deres kant.
-        Formålet er finne ut hvilke oppgaver som kan gjøre hvis det legges til rette. Dere kan endre planen når som helst etter hvert som dere ser hvordan det går.
+        Formålet er finne ut hvilke oppgaver som kan gjøres hvis det legges til rette. Dere kan endre planen når som helst etter hvert som dere ser hvordan det går.
     `,
     readMore: {
         title: 'Les mer om:',
