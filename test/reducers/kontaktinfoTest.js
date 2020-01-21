81f2abf0dbdd9e2b09d615321cabf61905dfae27
John Martin Lindseth <john.martin.lindseth@nav.no>
2020-01-21 14:19:42 +0100

    Fix filter methods in kontaktinfo reducer
    Når man endrer state på et fnr (f.eks. går fra henter til hentingFeilet) skal fødselsnummeret legges til i riktig liste i reduceren, og fjernes fra den forrige.
    Frem til nå, har det vært feil i filter-metodene, som gjør at et fnr aldri fjernes fra den listen det opprinnelig ble lagt i.
    
    Dette har gjort at når et kall har feilet, har fødselsnummeret blitt liggende i henter-listen, og vi har fått evig spinner.

--- test/reducers/kontaktinfoTest.js
+++ test/reducers/kontaktinfoTest.js
@@ -63,4 +63,121 @@ describe('kontaktinfo', () => {
             });
         });
     });
+
+    describe('henter, med kall som feiler og så går gjennom', () => {
+        it('HENTER_KONTAKTINFO skal fjerne fnr fra hentingFeilet-listen', () => {
+            const state = deepFreeze({
+                henter: [],
+                hentet: [],
+                hentingFeilet: ['fnr'],
+                data: [],
+            });
+
+            const action = actions.henterKontaktinfo('fnr');
+            const nextState = kontaktinfo(state, action);
+
+            expect(nextState).to.deep.equal({
+                henter: ['fnr'],
+                hentet: [],
+                hentingFeilet: [],
+                data: [],
+            });
+        });
+
+        it('HENT_KONTAKTINFO_FEILET Skal fjerne fnr fra henter-lister', () => {
+            const state = deepFreeze({
+                henter: ['fnr'],
+                hentet: [],
+                hentingFeilet: [],
+                data: [],
+            });
+
+            const action = actions.hentKontaktinfoFeilet('fnr');
+            const nextState = kontaktinfo(state, action);
+
+            expect(nextState).to.deep.equal({
+                henter: [],
+                hentet: [],
+                hentingFeilet: ['fnr'],
+                data: [],
+            });
+        });
+
+        it('KONTAKTINFO_HENTET skal fjerne fnr fra henter-listen', () => {
+            const state = deepFreeze({
+                henter: ['fnr1'],
+                hentet: [],
+                hentingFeilet: ['fnr2'],
+                data: [],
+            });
+
+            const action = actions.kontaktinfoHentet({
+                fnr: 'fnr1',
+                tlf: '123',
+                epost: 'test@nav.no',
+            }, 'fnr1',
+            );
+            const nextState = kontaktinfo(state, action);
+
+            expect(nextState).to.deep.equal({
+                henter: [],
+                hentet: ['fnr1'],
+                hentingFeilet: ['fnr2'],
+                data: [{
+                    kontaktinfo: {
+                        fnr: 'fnr1',
+                        tlf: '123',
+                        epost: 'test@nav.no',
+                    },
+                    fnr: 'fnr1',
+                }],
+            });
+        });
+
+        it('KONTAKTINFO_HENTET skal fjerne fnr fra hentingFeilet-listen', () => {
+            const state = deepFreeze({
+                henter: [],
+                hentet: ['fnr1'],
+                hentingFeilet: ['fnr2'],
+                data: [{
+                    kontaktinfo: {
+                        fnr: 'fnr1',
+                        tlf: '123',
+                        epost: 'test@nav.no',
+                    },
+                    fnr: 'fnr1',
+                }],
+            });
+
+            const action = actions.kontaktinfoHentet({
+                fnr: 'fnr2',
+                tlf: '123',
+                epost: 'test@nav.no',
+            }, 'fnr2',
+            );
+            const nextState = kontaktinfo(state, action);
+
+            expect(nextState).to.deep.equal({
+                henter: [],
+                hentet: ['fnr1', 'fnr2'],
+                hentingFeilet: [],
+                data: [{
+                    kontaktinfo: {
+                        fnr: 'fnr1',
+                        tlf: '123',
+                        epost: 'test@nav.no',
+                    },
+                    fnr: 'fnr1',
+                },
+                {
+                    kontaktinfo: {
+                        fnr: 'fnr2',
+                        tlf: '123',
+                        epost: 'test@nav.no',
+                    },
+                    fnr: 'fnr2',
+                }],
+            });
+        });
+    });
 });
