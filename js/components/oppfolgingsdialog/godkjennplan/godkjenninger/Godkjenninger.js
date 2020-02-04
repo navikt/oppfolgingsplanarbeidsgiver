4ca06a6660e5b0533afcbe3fd104820e6a30d59d
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-02-04 09:10:58 +0100

    Fix bug with invalid state when changing leader

--- js/components/oppfolgingsdialog/godkjennplan/godkjenninger/Godkjenninger.js
+++ js/components/oppfolgingsdialog/godkjennplan/godkjenninger/Godkjenninger.js
@@ -6,8 +6,9 @@ import { oppfolgingsplanPt } from '../../../../proptypes/opproptypes';
 
 const harMottattGodkjenninger = (oppfolgingsplan) => {
     const godkjenninger = oppfolgingsplan.godkjenninger;
-    const aktoer = oppfolgingsplan.arbeidsgiver.naermesteLeder;
-    return godkjenninger.length > 0 && godkjenninger[0].godkjentAv.fnr && godkjenninger[0].godkjentAv.fnr !== aktoer.fnr;
+    const arbeidstaker = oppfolgingsplan.arbeidstaker;
+    const godkjentAvFnr = godkjenninger.length > 0 && godkjenninger[0].godkjentAv.fnr;
+    return godkjentAvFnr && godkjentAvFnr === arbeidstaker.fnr;
 };
 
 const Godkjenninger = (
