5d4a326058956aaa514428a7c8849fa909fc9b62
John Martin Lindseth <john.martin.lindseth@nav.no>
2020-03-27 16:35:06 +0100

    Revert "Add information box regarding technical difficulties (#97)"
    This reverts commit 3b3ed41d

--- js/components/oppfolgingsdialog/Oppfolgingsdialog.js
+++ js/components/oppfolgingsdialog/Oppfolgingsdialog.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Alertstripe from 'nav-frontend-alertstriper';
 import { sykeforlopsPerioderReducerPt } from '@navikt/digisyfo-npm';
 import * as opProptypes from '../../proptypes/opproptypes';
 import {
@@ -22,18 +21,6 @@ import Samtykke from '../oppfolgingsdialog/godkjennplan/samtykke/Samtykke';
 import Godkjenn from './godkjennplan/Godkjenn';
 import Godkjenninger from './godkjennplan/godkjenninger/Godkjenninger';
 import ReleasetPlan from './godkjennplan/releasetplan/ReleasetPlan';
-
-const midlertidigTekstFastlegedeling = 'Det er for tiden ikke mulig å dele oppfølgingsplaner med fastlegen på grunn av endringer '
-                                     + 'i registre utenfor NAV som ikke er varslet. Vi jobber med å tilpasse våre systemer.';
-
-const MidlertidigAlertStripeFastlegedeling = () => {
-    return (<Alertstripe
-        className="alertstripe--notifikasjonboks"
-        type="info"
-        solid>
-        {midlertidigTekstFastlegedeling}
-    </Alertstripe>);
-};
 
 const manglerSamtykke = (oppfolgingsplan) => {
     return oppfolgingsplan.arbeidsgiver.naermesteLeder.samtykke === null;
@@ -189,7 +176,6 @@ class Oppfolgingsdialog extends Component {
         return (
             <div className="oppfolgingsdialog">
                 { oppfolgingsdialogAvbruttOgNyOpprettet && <AvbruttGodkjentPlanVarsel /> }
-                <MidlertidigAlertStripeFastlegedeling />
                 <SideOverskrift tittel={oppfolgingsdialog.arbeidstaker.navn} />
                 { !disableNavigation && <NavigasjonsTopp
                     disabled={disableNavigation}
