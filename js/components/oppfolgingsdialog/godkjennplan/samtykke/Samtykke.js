7de65fdd467169c5b48eb572ab4bd5c5015daba9
Tor-Christian Tangenes <torchrct@gmail.com>
2020-12-22 15:28:11 +0100

    Fjern godkjenn-hash etter samtykke

--- js/components/oppfolgingsdialog/godkjennplan/samtykke/Samtykke.js
+++ js/components/oppfolgingsdialog/godkjennplan/samtykke/Samtykke.js
@@ -54,6 +54,8 @@ class Samtykke extends Component {
 
     sendSamtykke() {
         const { oppfolgingsdialog } = this.props;
+        window.sessionStorage.removeItem('hash');
+        window.location.hash = '';
         this.props.sendSamtykke(oppfolgingsdialog.id, this.state.samtykke, oppfolgingsdialog.arbeidstaker.fnr, erArbeidstakerEgenLeder(oppfolgingsdialog));
     }
 
