3b9c9c386b97bd50cbdfe214389232248aa5ccfa
Tor-Christian Tangenes <torchrct@gmail.com>
2021-12-09 21:09:06 +0100

    Split opp test, og legg til en ny for å sjekke at alertboks er tilstedet når lagring av oppgave feiler

--- test/jest/Inputfelt.test.js
+++ test/jest/Inputfelt.test.js
@@ -2,29 +2,29 @@ import '@testing-library/jest-dom';
 import React from 'react';
 import { screen, render, fireEvent } from '@testing-library/react';
 import  Inputfelt, { texts } from '../../js/skjema/Inputfelt';
+  
+
+const inputRole = 'input';
+const submitRole = 'submit';
+const alertRole = 'alert';
 
 test('Skal vise riktig feilmeldinger ved validering', () => {
-  const onSubmit = jest.fn()
-  const avbryt = jest.fn()
+  const onSubmit = jest.fn();
+  const avbryt = jest.fn();
 
-  const validString = 'Valid string'
-  const emptyString = ''
-  const invalidString = '?? Invalid string'
-  var tooLongString = ''
+  const emptyString = '';
+  const invalidString = '?? Invalid string';
+  var tooLongString = '';
   for (var i = 0; i < 101; i++) {
     tooLongString += 'C';
   }
-
-  const inputRole = 'input';
-  const submitRole = 'submit';
-  const alertRole = 'alert';
 
   render(<Inputfelt
     oppdateringFeilet={false}
     spinner={false}
     onSubmit={onSubmit}
     avbryt={avbryt}
-  />)
+  />);
 
   const textareaInput = screen.getByRole(inputRole);
   const submitButton = screen.getByRole(submitRole);
@@ -43,9 +43,42 @@ test('Skal vise riktig feilmeldinger ved validering', () => {
   fireEvent.click(submitButton);
   const errorTextTooLong = screen.getByText(texts.errors.maxLengthExceeded);
   expect(errorTextTooLong).toBeInTheDocument();
+});
+
+test('Skal IKKE vise feilmelding dersom input er gyldig', () => {
+  const onSubmit = jest.fn();
+  const avbryt = jest.fn();
+
+  const validString = 'Valid string';
+
+  render(<Inputfelt
+    oppdateringFeilet={false}
+    spinner={false}
+    onSubmit={onSubmit}
+    avbryt={avbryt}
+  />);
+
+  const textareaInput = screen.getByRole(inputRole);
+  const submitButton = screen.getByRole(submitRole);
 
   fireEvent.change(textareaInput, { target: { value: validString}});
   fireEvent.click(submitButton);
-  const errorText = screen.getByRole(alertRole);
-  expect(errorText).not.toHaveTextContent();
+  const errorPanel = screen.getByRole(alertRole);
+  expect(errorPanel).not.toHaveTextContent();
+});
+
+test('Skal vise alertboks dersom lagring av ny arbeidsoppgave feiler', () => {
+  const onSubmit = jest.fn();
+  const avbryt = jest.fn();
+
+  const alertRole = 'alert';
+
+  render(<Inputfelt
+    oppdateringFeilet={true}
+    spinner={false}
+    onSubmit={onSubmit}
+    avbryt={avbryt}
+  />);
+
+  expect(screen.getByText(texts.errors.update)).toBeInTheDocument();
 });
