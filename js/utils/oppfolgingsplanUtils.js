a742d936f99f56c9fdd1c083caf54dd1d878005a
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2022-03-24 13:36:58 +0100

    Ramove date check from non-approved plans

--- js/utils/oppfolgingsplanUtils.js
+++ js/utils/oppfolgingsplanUtils.js
@@ -216,6 +216,7 @@ export const finnGyldigePlanerPaVirksomhet = (planer, orgnummer, dineSykmeldteMe
         oppfolgingsplan.godkjentPlan.gyldighetstidspunkt.tom
       );
     }
+    return true
   });
 };
 
